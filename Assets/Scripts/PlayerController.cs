6c016c9e51cf29b7b45149beeab2a7f768019781
Harish Bhagat <h.bhagat@hotmail.co.uk>
2017-06-07 15:34:02 +0100

    Animations & Audio Assets Added
    Animations, audio assets and the basic outline of the game have been
    added.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	// Variables
+	private Animator _animator;
+
+	private void Start()
+	{
+		// Cache components
+		_animator = GetComponent<Animator>();
+	}
+
+	private void Update()
+	{
+		// Check for input
+		if (Input.GetKeyDown(KeyCode.W))
+		{
+			_animator.Play("Jump Up");
+		}
+		else if (Input.GetKeyDown(KeyCode.S))
+		{
+			_animator.Play("Jump Down");
+		}
+		else if (Input.GetKeyDown(KeyCode.A))
+		{
+			_animator.Play("Jump Left");
+		}
+		else if (Input.GetKeyDown(KeyCode.D))
+		{
+			_animator.Play("Jump Right");
+		}
+	}
+}
